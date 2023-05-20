79778a6f8720de3dec24d282b93492f0889fb3eb
Huy Vũ <93518371+huyvu23@users.noreply.github.com>
2023-05-20 12:40:39 +0700

    [ADD] - fix base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://huyvu23.github.io/MyCV/",
+  base: "/<MyCV>/",
 })
